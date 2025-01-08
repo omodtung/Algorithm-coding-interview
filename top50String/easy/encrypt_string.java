5d2177ebaf97c8e721b73bb1dd5f5c42219085d2
TungDT <dothedung131@gmail.com>
2025-01-08 23:40:57 +0700

    fix

--- top50String/easy/encrypt_string.java
+++ top50String/easy/encrypt_string.java
@@ -1,18 +1,11 @@
 public class encrypt_string {
-    
-    static String encryptTheString ( String s, int N )
-    {
-        StringBuilder ans = new StringBuilder() ;
-        for ( int i =0 ; i <N  ;i++)
-        {
-            char ch = s.charAt(i) ;
-            int count = 0 ;
-            String hex ;
-            while ( i< N && s.) {
-                
-            }
 
-
-        }
+  static String encryptTheString(String s, int N) {
+    StringBuilder ans = new StringBuilder();
+    for (int i = 0; i < N; i++) {
+      char ch = s.charAt(i);
+      int count = 0;
+      String hex;
     }
+  }
 }
